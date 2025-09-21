42e91cf3192a9064fd153930238ce83e0cd0be34
vaso.vucinic <vasoct@gmail.com>
2025-09-21 15:56:03 +0200

    view class

--- app/src/main/java/app/vucinicvaso/resumebuilderapp/MainActivity.kt
+++ app/src/main/java/app/vucinicvaso/resumebuilderapp/MainActivity.kt
@@ -18,13 +18,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             ResumeBuilderAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    innerPadding ->
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
                 }
             }
         }
---
+++ app/src/main/java/app/vucinicvaso/resumebuilderapp/core/base/view/WTView.kt
@@ -0,0 +1,9 @@
+package app.vucinicvaso.resumebuilderapp.core.base.view
+
+import androidx.activity.ComponentActivity
+
+class WTView : ComponentActivity() {
+
+
+
+}
