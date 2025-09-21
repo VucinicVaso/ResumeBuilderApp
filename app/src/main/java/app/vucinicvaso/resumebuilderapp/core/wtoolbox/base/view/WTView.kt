5294093fc94a5dd7a7e833d1c6bedd33142fdc88
vaso.vucinic <vasoct@gmail.com>
2025-09-21 16:24:24 +0200

    updated ViewModel and View classes

--- app/src/main/java/app/vucinicvaso/resumebuilderapp/core/wtoolbox/base/view/WTView.kt
+++ app/src/main/java/app/vucinicvaso/resumebuilderapp/core/wtoolbox/base/view/WTView.kt
@@ -1,9 +1,20 @@
 package app.vucinicvaso.resumebuilderapp.core.wtoolbox.base.view
 
 import androidx.activity.ComponentActivity
+import app.vucinicvaso.resumebuilderapp.core.wtoolbox.uifactory.factory.WTUIFactory;
+import app.vucinicvaso.resumebuilderapp.core.wtoolbox.uifactory.factory.impl.WTUIFactoryImpl;
 
-abstract class WTView : ComponentActivity() {
+abstract class WTView<VM>(viewModel: VM?) : ComponentActivity() {
 
+    init {
+        setUiFactory();
+    }
 
+    var viewModel : VM? = viewModel;
+
+    var uiFactory : WTUIFactory? = null;
+    fun setUiFactory() {
+        uiFactory = WTUIFactoryImpl.instance;
+    }
 
 }
---
+++ app/src/main/java/app/vucinicvaso/resumebuilderapp/core/wtoolbox/base/viewmodel/WTViewModel.kt
@@ -0,0 +1,3 @@
+package app.vucinicvaso.resumebuilderapp.core.wtoolbox.base.viewmodel
+
+abstract class WTViewModel {}
